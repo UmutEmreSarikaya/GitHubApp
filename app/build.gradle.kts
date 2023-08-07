5706f85b4086a9a772fd4664d81f35b17d2b1e7d
umutsarikaya <u.emresarikaya@gmail.com>
2023-08-07 21:50:59 +0300

    Added Bottom Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
 
     buildFeatures{
         dataBinding = true
+        viewBinding = true
     }
 
     defaultConfig {
@@ -71,6 +72,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.8.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
 
     implementation ("com.airbnb.android:lottie:6.1.0")
 
--- app/src/main/java/com/umutemregithub/app/ui/MainActivity.kt
+++ app/src/main/java/com/umutemregithub/app/ui/MainActivity.kt
@@ -2,13 +2,24 @@ package com.umutemregithub.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.umutemregithub.app.R
+import com.umutemregithub.app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }

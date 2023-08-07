d20e81706e82f8fcb5b87729d33bdff7223e06f1
umutsarikaya <u.emresarikaya@gmail.com>
2023-08-07 14:52:27 +0300

    Initial Commit

---
+++ app/src/main/java/com/umutemregithub/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.umutemregithub.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

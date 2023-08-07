cbd5bacceec9ee9f4e483e4cce63ea36161083d2
umutsarikaya <u.emresarikaya@gmail.com>
2023-08-07 16:13:25 +0300

    Splash Screen Added

--- app/src/main/java/com/umutemregithub/app/ui/MainActivity.kt
+++ app/src/main/java/com/umutemregithub/app/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.umutemregithub.app
+package com.umutemregithub.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.umutemregithub.app.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
